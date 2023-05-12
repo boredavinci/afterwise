74b3c2d413f2b55bd02b06045b7e4ce58be20125
@boredavinci <boredavinci@gmail.com>
2023-05-12 21:22:54 +0100

    Initial commit from Create Next App

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,25 @@
-import Navbar from '@/components/Navbar'
-import './globals.css'
-import { Raleway } from 'next/font/google'
+import Navbar from "@/components/Navbar";
+import "./globals.css";
+import { Raleway } from "next/font/google";
 
-const raleway = Raleway({ subsets: ['latin'] })
+const raleway = Raleway({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Afterwise',
-  description: 'Trustless end of life digital legacy management',
-}
+  title: "Afterwise",
+  description: "Trustless end of life digital legacy management",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={raleway.className}>
         <Navbar />
-        {children}</body>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 export default function Home() {
   return (
-<div className="hero min-h-screen" style={{ backgroundImage: `url("/bg.jpg")` }}>
-  <div className="hero-overlay bg-opacity-60">
-  </div>
-  <div className="hero-content text-center text-neutral-content">
-    <div className="max-w-2xl">
-      <p className="mb-6 text-5xl">Trustless end of life digital legacy management </p>
-      <button className="btn btn-primary">Connect Wallet</button>
+    <div
+      className="hero min-h-screen"
+      style={{ backgroundImage: `url("/bg.jpg")` }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-2xl">
+          <p className="mb-6 text-5xl">
+            Trustless end of life digital legacy management{" "}
+          </p>
+          <button className="btn btn-primary">Connect Wallet</button>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-  )
+  );
 }
