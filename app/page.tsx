5556f38f118228045131d58af09425a9634bc0b3
Bruno Wu <brunowu2000@gmail.com>
2023-05-14 01:21:08 +0100

    Update title

--- app/page.tsx
+++ app/page.tsx
@@ -71,9 +71,7 @@ export default function Home() {
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-2xl">
-          <p className="mb-6 text-5xl">
-            Trustless end of life digital legacy management{" "}
-          </p>
+          <p className="mb-6 text-5xl">Trustless Inheritance Vaults</p>
           {safeAddress ? (
             <p>{safeAddress}</p>
           ) : (
